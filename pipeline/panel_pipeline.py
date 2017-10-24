59f16c2656cb4f2e07f9dc1cf71a23c30c0bd98d
chie-furusawa <chie_furusawa@dwango.co.jp>
2017-10-24 15:10:35 +0900

    fix pipeline class to use without sr (#112)
    * fix_pipeline_class_to_use_without_sr

--- pipeline/panel_pipeline.py
+++ pipeline/panel_pipeline.py
@@ -59,7 +59,7 @@ class PanelPipeline(object):
         # type: (comicolorization.drawer.Drawer, comicolorization_sr.drawer.Drawer, any, any, any, any) -> None
         """
         :param drawer: drawer of the comicolorization task
-        :param drawer_sr: draw of the super resolution task
+        :param drawer_sr: draw of the super resolution task, None-> skip to super-resolution
         :param image: source panel iamge
         :param reference_image: reference image
         :param resize_width: width of input image for neural network model
@@ -99,10 +99,16 @@ class PanelPipeline(object):
             input_width=self.resize_width,
         )
 
-        self._crop_pre = _calc_input_panel_rect(
-            panel_size=self.image.size,
-            input_width=self.resize_width * 2,
-        )
+        if self.drawer_sr is not None:
+            self._crop_pre = _calc_input_panel_rect(
+                panel_size=self.image.size,
+                input_width=self.resize_width * 2,
+            )
+        else:
+            self._crop_pre = _calc_input_panel_rect(
+                panel_size=self.image.size,
+                input_width=self.resize_width,
+            )
 
         small_input_image = make_binarized_image(input_panel_image, self.threshold)
         big_input_image = _make_input_panel_image(self.image, self._crop_pre, self.resize_width * 2)
@@ -110,8 +116,6 @@ class PanelPipeline(object):
         return small_input_image, big_input_image
 
     def _draw_process(self, small_input_image, big_input_image):
-        concat_image_process = self.drawer_sr.colorization.get_concat_process()
-
         lab = rgb2lab(numpy.array(small_input_image))
         lab[:, :, 0] /= 100
         small_image = self.drawer.draw(
@@ -120,17 +124,26 @@ class PanelPipeline(object):
         )[0]
 
         small_image = small_image.convert('RGB')
+
+        if self.drawer_sr is not None:
+            drawn_panel_image = self._superresolution_process(small_image, big_input_image)
+        else:
+            drawn_panel_image = small_image
+
+        return drawn_panel_image
+
+    def _superresolution_process(self, small_image, big_input_image):
         small_array = numpy.array(small_image, dtype=numpy.float64)
         small_array = rgb2lab(small_array / 255).astype(numpy.float32)
         small_array = small_array.transpose(2, 0, 1) / 100
-
+        concat_image_process = self.drawer_sr.colorization.get_concat_process()
         large_array = concat_image_process(big_input_image, test=True)
-        drawn_panel_image = self.drawer_sr.draw_only_super_pixel(
+        sr_drawn_panel_image = self.drawer_sr.draw_only_super_pixel(
             image=small_array[numpy.newaxis],
             concat=large_array[numpy.newaxis],
         )[0]
 
-        return drawn_panel_image
+        return sr_drawn_panel_image
 
     def _post_process(self, drawn_panel_image):
         """
@@ -143,7 +156,7 @@ class PanelPipeline(object):
         array[(array < th).all(axis=2)] = numpy.ones(3) * 0
         array[(array > 255 - th).all(axis=2)] = numpy.ones(3) * 255
         image = Image.fromarray(array)
-
         image = image.crop(self._crop_pre)
         image = image.resize(self.image.size, Image.BICUBIC)
+
         return image
